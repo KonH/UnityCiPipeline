f326255f0fdf4879f6ee97b988bb16caeefaab79
KonH <konh@yandex.ru>
2019-05-27 22:37:11 +0700

    Build pipeline basics

---
+++ Assets/Scripts/Editor/CustomBuildPipeline.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class CustomBuildPipeline : MonoBehaviour {
+	[MenuItem("BuildPipeline/RunBuild")]
+	public static void RunBuild() {
+		var opts = new BuildPlayerOptions {
+			target           = BuildTarget.WebGL,
+			targetGroup      = BuildTargetGroup.WebGL,
+			locationPathName = "Build"
+		};
+		BuildPipeline.BuildPlayer(opts);
+		
+		// later:
+		// butler push /Volumes/SamsungSSD/Projects/UnityCiPipeline/Build konh/test-ci:html
+	}
+}
