3505508a30352a83adb86f74cf0906490b842924
KonH <konh@yandex.ru>
2019-05-28 21:28:36 +0700

    Build with custom version

--- Assets/Scripts/Editor/CustomBuildPipeline.cs
+++ Assets/Scripts/Editor/CustomBuildPipeline.cs
@@ -1,17 +1,36 @@
-﻿using UnityEditor;
+﻿using System;
+using System.Linq;
+using UnityEditor;
 using UnityEngine;
 
-public class CustomBuildPipeline : MonoBehaviour {
-	[MenuItem("BuildPipeline/RunBuild")]
-	public static void RunBuild() {
-		var opts = new BuildPlayerOptions {
-			target           = BuildTarget.WebGL,
-			targetGroup      = BuildTargetGroup.WebGL,
-			locationPathName = "Build"
-		};
-		BuildPipeline.BuildPlayer(opts);
-		
-		// later:
-		// butler push /Volumes/SamsungSSD/Projects/UnityCiPipeline/Build konh/test-ci:html
+namespace UnityCiPipeline {
+	public class CustomBuildPipeline : MonoBehaviour {
+		[MenuItem("BuildPipeline/RunBuild")]
+		public static void RunBuild() {
+			var opts = new BuildPlayerOptions {
+				target           = BuildTarget.WebGL,
+				targetGroup      = BuildTargetGroup.WebGL,
+				locationPathName = "Build"
+			};
+			BuildPipeline.BuildPlayer(opts);
+		}
+
+		public static void RunBuildForVersion() {
+			var commitHash = GetVersion();
+			Debug.Log($"RunBuildForCommit: version='{commitHash}'");
+			PrepareForBuild(commitHash);
+			RunBuild();
+		}
+
+		static string GetVersion() {
+			return Environment.GetCommandLineArgs()
+				.Where(a => a.StartsWith("-version="))
+				.Select(a => a.Remove(0, "-version=".Length))
+				.First();
+		}
+
+		static void PrepareForBuild(string version) {
+			PlayerSettings.bundleVersion = version;
+		}
 	}
 }
