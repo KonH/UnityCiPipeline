add9ff980499f3cd77a016a78ebe284bbf40a907
KonH <konh@yandex.ru>
2019-06-04 17:37:56 +0700

    Update docker script

--- Docker/docker_build.sh
+++ Docker/docker_build.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/KonH/UnityCiPipeline project
 cd project
+./build.sh -target=Decode-License-File
 ./build.sh -target=Use-Manual-Activation-File
 ./build.sh -target=Build
