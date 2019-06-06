cc1d968372e3d3d8b60a97114ee6e6ad858cd45b
KonH <konh@yandex.ru>
2019-06-06 20:30:35 +0700

    Ability to change build target

--- Assets/Scripts/Editor/CustomBuildPipeline.cs
+++ Assets/Scripts/Editor/CustomBuildPipeline.cs
@@ -6,21 +6,30 @@ using UnityEngine;
 namespace UnityCiPipeline {
 	public class CustomBuildPipeline : MonoBehaviour {
 		[MenuItem("BuildPipeline/RunBuild")]
-		public static void RunBuild() {
+		public static void RunBuild(BuildTarget target) {
+			var targetGroup = BuildPipeline.GetBuildTargetGroup(target);
 			var opts = new BuildPlayerOptions {
-				target           = BuildTarget.WebGL,
-				targetGroup      = BuildTargetGroup.WebGL,
+				target           = target,
+				targetGroup      = targetGroup,
 				locationPathName = "Build",
 				scenes           = EditorBuildSettings.scenes.Where(s => s.enabled).Select(s => s.path).ToArray()
 			};
 			BuildPipeline.BuildPlayer(opts);
 		}
 
+		[MenuItem("BuildPipeline/RunBuild/WebGL")]
+		public static void RunBuild_WebGL() => RunBuild(BuildTarget.WebGL);
+
 		public static void RunBuildForVersion() {
+			var targetVariable = Environment.GetEnvironmentVariable("BUILD_TARGET");
+			if ( string.IsNullOrEmpty(targetVariable) ) {
+				throw new InvalidOperationException("BUILD_TARGET isn't provided!");
+			}
+			var target = (BuildTarget)Enum.Parse(typeof(BuildTarget), targetVariable);
 			var commitHash = GetVersion();
 			Debug.Log($"RunBuildForCommit: version='{commitHash}'");
 			PrepareForBuild(commitHash);
-			RunBuild();
+			RunBuild(target);
 		}
 
 		static string GetVersion() {
